f6a62c46ba3873804ef66d3c97b2f98c33c37b18
danghaonhien <58311274+danghaonhien@users.noreply.github.com>
2020-02-28 22:18:32 -0800

    add responsive

--- homescript.js
+++ homescript.js
@@ -58,3 +58,9 @@ var TxtRotate = function(el, toRotate, period) {
 $('.ui.labeled.icon.sidebar')
   .sidebar('toggle')
 ;
+
+//for screen
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
